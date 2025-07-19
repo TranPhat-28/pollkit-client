ee6b2d847540b660a27d4b49cfb5ce58fe3c4d57
hdang-dev <haidang.fordevelop@gmail.com>
2025-07-19 22:21:29 +0700

    feat: update create poll page

--- src/app/create-poll/page.tsx
+++ src/app/create-poll/page.tsx
@@ -1,37 +1,52 @@
+"use client";
+import { useState } from "react";
+
+type PollType = "public" | "private"; // remove later
+
 const CreatePollPage = () => {
+  const [pollType, setPollType] = useState<PollType>("public");
+  const selectPollType = (type: PollType) => {
+    setPollType(type);
+  };
   return (
-    <div className="h-full w-full p-4 md:p-5 lg:p-6 shadow-lg mt-6 rounded">
-      <p className="font-bold text-2xl mb-5">New Poll</p>
-      <form action="" className="flex flex-col gap-4">
-        <div className="flex w-full justify-between items-center gap-4">
-          <span>Title</span>
-          <input type="text" placeholder="Poll title" className="input" />
+    <div className="h-full w-full p-4 md:p-5 lg:p-6">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-[600px] border p-4 mx-auto">
+        <legend className="fieldset-legend text-lg">New Poll</legend>
+
+        <label className="label text-base">Title</label>
+        <input type="text" required className="input w-full" />
+
+        <label className="label text-base">Time to end</label>
+        <input type="time" className="input w-full" />
+
+        <label className="label text-base">Type</label>
+        <select
+          className="select w-full"
+          onChange={(a) => selectPollType(a.target.value as PollType)}
+        >
+          <option value="public">Public poll</option>
+          <option value="private">Private poll</option>
+        </select>
+
+        {pollType === "private" && (
+          <>
+            <label className="label text-base">Password</label>
+            <input type="password" required className="input w-full" />
+          </>
+        )}
+
+        <label className="label text-base ">Show result to users</label>
+        <div className="h-10 flex items-center">
+          <input type="checkbox" className="toggle" />
         </div>
-        <div className="flex w-full justify-between items-center gap-4">
-          <span>Type</span>
-          <select defaultValue="Public poll" className="select">
-            <option disabled={true}>Public poll</option>
-            <option>Private poll</option>
-          </select>
+
+        <label className="label text-base">Record for review</label>
+        <div className="h-10 flex items-center">
+          <input type="checkbox" className="toggle" />
         </div>
-        <div className="flex w-full justify-between items-center gap-4">
-          <span>Password</span>
-          <input type="text" placeholder="Password" className="input" />
-        </div>
-        <div className="flex w-full justify-between items-center gap-4">
-          <span>End time</span>
-          <input type="time" className="input" />
-        </div>
-        <div className="flex w-full justify-between items-center gap-4 min-h-10">
-          <span>Record result</span>
-          <input type="checkbox" defaultChecked className="toggle" />
-        </div>
-        <div className="flex w-full justify-between items-center gap-4 min-h-10">
-          <span>Result visible</span>
-          <input type="checkbox" defaultChecked className="toggle" />
-        </div>
-        <button className="btn mt-4 max-w-[200px]">Create</button>
-      </form>
+
+        <button className="btn btn-neutral mt-4">Login</button>
+      </fieldset>
     </div>
   );
 };
