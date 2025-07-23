b0da4f9e76da535478e572b6b18c38a2b0918f3f
TranPhat-28 <tphat8301@gmail.com>
2025-07-23 23:08:45 +0700

    refactor: Update all to client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from "@/components/Navbar";
 import SidebarDrawer from "@/components/SidebarDrawer";
 import type { Metadata } from "next";
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { FaFacebook, FaGoogle } from "react-icons/fa6";
 
 const LoginPage = () => {
